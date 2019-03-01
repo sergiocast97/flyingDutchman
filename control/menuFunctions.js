1e66ec02e60bf46f70874bab840d850bc56b9d98
Davide0295 <davide.rendina@gmail.com>
2019-03-01 11:47:11 +0100

    Menu page

---
+++ control/menuFunctions.js
@@ -0,0 +1,24 @@
+$("#tabBeers").click(function() {
+    $('html,body').animate({
+            scrollTop: $("#Beers").offset().top},
+    500, 'linear');
+});
+
+$("#tabCiders").click(function() {
+    $('html,body').animate({
+            scrollTop: $("#Ciders").offset().top},
+        500, 'linear');
+});
+
+$("#tabSpirits").click(function() {
+    $('html,body').animate({
+            scrollTop: $("#Spirits").offset().top},
+        500, 'linear');
+});
+
+$("#tabWines").click(function() {
+    $('html,body').animate({
+            scrollTop: $("#Wines").offset().top},
+        500, 'linear');
+});
+
