1c5397ab50abd7d869b38968883cbf601668357e
Matilda Trodin <matilda@trodin.com>
2019-03-05 19:32:56 +0100

    started on login

---
+++ control/login.js
@@ -0,0 +1,5 @@
+function login(){
+    var uname = document.getElementById("uname").value;
+    var pasw = document.getElementById("psw").value;
+    var submit = document.getElementById("submit").disabled = true;
+}
