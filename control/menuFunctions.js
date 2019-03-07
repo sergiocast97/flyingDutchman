292e422fe2c5f69096b0621693db870651bd9f5b
Davide0295 <davide.rendina@gmail.com>
2019-03-07 10:39:16 +0100

    Working on a smooth scroll to div function

--- control/menuFunctions.js
+++ control/menuFunctions.js
@@ -1,3 +1,4 @@
+//Function to switch panel
 $(function() {
 
     $('.tabPanel .tabs li').on('click', function() {
@@ -23,5 +24,18 @@ $(function() {
         }
     });
 
+
+});
+
+
+//Scroll to div function
+$(function () {
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
 
 });
