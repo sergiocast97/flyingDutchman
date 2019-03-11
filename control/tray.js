4327bd761b18cc96a779e9b0caaaa0bed32e5bf5
Matilda Trodin <matilda@trodin.com>
2019-03-11 13:26:41 +0100

    You can now see the total amount to pay

--- control/tray.js
+++ control/tray.js
@@ -2,12 +2,12 @@ function addToTray(beverage){
 
     //here we should get the tray-element instead, have to create it in the menu.HTML
     var ul = document.getElementById("tray");
-
     var li = document.createElement("li");
+
     li.id = beverage[3];
     li.innerHTML = "<span class=brand>" + beverage[0] + "</span>" + " " + beverage[1] + " " + "<span class=price>" + beverage[4] + "kr" + "</span>";
     li.className = "trayItem";
-    li.addEventListener('click', () => removeFromTray(li));
+    li.addEventListener('click', () => {removeFromTray(li);subAmount(beverage[4])});
     //ul.value = "";
     ul.appendChild(li);
 
@@ -16,5 +16,30 @@ function addToTray(beverage){
 function removeFromTray(beverage){
     var ul = document.getElementById("tray");
     ul.removeChild(beverage)
+}
+
+function addAmount(beverage){
+
+    var previousAmount = document.getElementById("amount").innerHTML;
+    console.log(previousAmount);
+    var totalAmount = +previousAmount + +beverage[4];
+    totalAmount = Math.round(totalAmount * 10)/10;
+
+    document.getElementById("amount").innerHTML = totalAmount;
+    console.log(totalAmount);
+
+}
+
+function subAmount(beverage){
+
+    var previousAmount = document.getElementById("amount").innerHTML;
+    console.log(beverage);
+    console.log(previousAmount);
+    var totalAmount = +previousAmount - +beverage;
+    totalAmount = Math.round(totalAmount * 10)/10;
+
+    document.getElementById("amount").innerHTML = totalAmount;
+    console.log(totalAmount);
+
 }
 
