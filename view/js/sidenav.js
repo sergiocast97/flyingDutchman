28dfb5a1aecdbe9e6fcb667d7078d779c3a856a3
sergiocast-97 <sergiocast97@gmail.com>
2019-03-22 13:46:40 +0100

    Finishing touches

--- view/js/sidenav.js
+++ view/js/sidenav.js
@@ -1,6 +1,7 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     $("#mySidenav").css('width','25%');
+    $(".navOpener").css('right','25%').attr('onclick','closeNav()');
     $(".dark").css({
         'visibility': 'visible',
         'opacity': '1',
@@ -10,6 +11,7 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     $("#mySidenav").css('width','0');
+    $(".navOpener").css('right','0').attr('onclick','openNav()');
     $(".dark").css({
         'visibility': 'hidden',
         'opacity': '0',
